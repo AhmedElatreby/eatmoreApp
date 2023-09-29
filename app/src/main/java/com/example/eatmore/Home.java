1bcf63c21860ef49128d36e66df03c73723d30a7
AhmedElatreby <ahmedsport@hotmail.com>
2023-09-29 22:15:46 +0100

    create home page stage 2

--- app/src/main/java/com/example/eatmore/Home.java
+++ app/src/main/java/com/example/eatmore/Home.java
@@ -1,14 +1,46 @@
 package com.example.eatmore;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Home extends AppCompatActivity {
+    RecyclerView recyclerCategories;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        recyclerCategories = findViewById(R.id.recycler_categories);
+        setCategories();
+    }
+
+    private void setCategories() {
+        List<FoodCategory> data = new ArrayList<>();
+
+        FoodCategory foodCategory = new FoodCategory("Chicken", R.drawable.ic_chicken);
+        FoodCategory foodCategory2 = new FoodCategory("Burger", R.drawable.burger);
+        FoodCategory foodCategory3 = new FoodCategory("Pizza", R.drawable.pizza);
+        FoodCategory foodCategory4 = new FoodCategory("Chicken", R.drawable.ic_chicken);
+        FoodCategory foodCategory5 = new FoodCategory("Chicken", R.drawable.ic_chicken);
+
+        data.add(foodCategory);
+        data.add(foodCategory2);
+        data.add(foodCategory3);
+        data.add(foodCategory4);
+        data.add(foodCategory5);
+
+        FoodCategoryAdapter foodCategoryAdapter = new FoodCategoryAdapter(data, Home.this);
+
+        recyclerCategories.setLayoutManager(new LinearLayoutManager(Home.this, RecyclerView.HORIZONTAL, false));
+        recyclerCategories.setAdapter(foodCategoryAdapter);
+
+        foodCategoryAdapter.notifyDataSetChanged();
     }
 }
